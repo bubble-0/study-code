2c5bef11c989439d2406c80567a82ef79b4b00e9
Bubble-0 <miaojing_0904@163.com>
2020-12-15 18:08:45 +0800

    1111

--- src/main/java/com/magic/springboot/TestApplication.java
+++ src/main/java/com/magic/springboot/TestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 引导类，spring boot的 入门类
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 //@EnableAutoConfiguration  启动自动配置,启动一次就可以了
 //@ComponentScan 开启自动扫描，只扫描该类所在的包和子包
 @SpringBootApplication //组合组件，相当于@EnableAutoConfigtiration  @ComponentScan  @SpringBootConfiguration的组合
+@EnableScheduling
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);}
